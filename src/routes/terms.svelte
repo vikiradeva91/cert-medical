4b46cfa8a59bbf61410d226171789210379d4ff6
Viki <viki.radeva91@gmail.com>
2021-10-18 16:02:37 +0300

    terms

--- src/routes/terms.svelte
+++ src/routes/terms.svelte
@@ -1,5 +1,14 @@
 <div class="rules">
     <section>
         <h1><i>―   TERMS OF USE   ―</i></h1>
+        <ol type="I">
+            <li>
+                GENERAL INFORMATION
+            </li>
+            <p>The content of the Website is for informational purpose and does not provide information society services within the meaning of the Electronic Commerce Act.</p>
+            <p>
+                This website is owned and operated by Cert Medical LTD, with its registered office and address: Tsar Boris III Blvd., Andromeda Business Center, 3rd floor, office 31 , tel .: +359 877 122 777, e-mail: petar.emilov.petrov@gmail.com represented by Petar Petrov.
+            </p>
+        </ol>
     </section>
 </div>
