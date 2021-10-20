e185c438a570aa3ee7f0488d270933588c3ded8e
Viki <viki.radeva91@gmail.com>
2021-10-20 15:57:28 +0300

    terms

--- src/routes/terms.svelte
+++ src/routes/terms.svelte
@@ -1,14 +1,98 @@
-<div class="rules">
-    <section>
-        <h1><i>―   TERMS OF USE   ―</i></h1>
-        <ol type="I">
-            <li>
-                GENERAL INFORMATION
-            </li>
-            <p>The content of the Website is for informational purpose and does not provide information society services within the meaning of the Electronic Commerce Act.</p>
-            <p>
-                This website is owned and operated by Cert Medical LTD, with its registered office and address: Tsar Boris III Blvd., Andromeda Business Center, 3rd floor, office 31 , tel .: +359 877 122 777, e-mail: petar.emilov.petrov@gmail.com represented by Petar Petrov.
-            </p>
-        </ol>
-    </section>
+<div class="content scroll">
+    <div class="rules">
+        <section>
+            <h1 style="padding-bottom: 15px;"><i>―   TERMS OF USE   ―</i></h1>
+            <ol type="I">
+                <li>GENERAL INFORMATION</li>
+                <p>The content of the Website is for informational purpose and does not provide information society services within the meaning of the Electronic Commerce Act.</p>
+                <p>This website is owned and operated by Cert Medical LTD. Cert Medical with its registered office and address: Sofia 1618, Bulgaria, 168 168 Tsar Boris III Blvd., Andromeda Business Center, 3rd floor, office 31 , tel .: +359 877 122 777, e-mail: petar.emilov.petrov@gmail.com represented by Petar Petrov.</p>
+                <li>USE OF THE WEBSITE</li>
+                <p>These General Terms are intended to govern all terms of use and access to the Website. You must familiarize yourself with these General Terms and Conditions and the Privacy Notice. If necessary, we reserve the right to unilaterally change the scope of the content and functionality of the Website. By changing the functionality of the Website, having legal or contractual obligations that apply to us, we may modify these general terms and conditions, which are made known to users by posting them here. This website may link to other sites or web sites administered by us.</p>
+                <li>LIMITATION OF LIABILITY</li>
+                <p>We are not responsible for damages and lost profits arising from the inability to provide the content or the inaccessibility of the website as a result of circumstances beyond our control. We are not responsible for any damages (including, but not limited to, losses and / or lost profits, missed business opportunities or projects, or loss of profits, etc.) resulting from the use or inability to use the Website, or any content extracted from it.</p>
+                <p>We are not responsible for damages caused by virus infection or any other software that has malicious properties when the prevention was beyond our control. Certain links from the site may lead to resources located on third-party servers or cloud structures over which the Administrator has no control and is not responsible for their content.</p>
+                <li>COPYRIGHT AND INTELLECTUAL PROPERTY</li>
+                <p>All graphic images and elements of the Website, the Database and other copyright objects are the exclusive property of Cert Medical LTD. The users / visitors of the site are not allowed to copy, reproduce or perform any actions that would infringe our copyright without our prior written consent.</p>
+                <li>SECURITY AND PROTECTION OF PERSONAL DATA</li>
+                <p>Our website uses the secure Hypertext Transfer Protocol Secure (HTTPS) protocol. We do not collect personal information through our site, but we may obtain it if you provide it to us by sending us an email message through the contact form or email. A certain set of personal data will be required for us to be able to provide feedback to you or to respond to your request.</p>
+                <p>Cert Medical LTD. endeavors to ensure the confidentiality and security of information, including the personal data of our employees and contractors, and persons who have provided personal information through our website. Max Graphic EOOD. continually improves technical and organizational measures, representing internal rules, policies and procedures that ensure that personal information is protected from loss, unauthorized access or disclosure.</p>
+                <li>OTHER TERMS</li>
+                <p>The outstanding Bulgarian and European law applicable to the particular case shall apply to the outstanding issues. All disputes arising out of the use of the web site shall be settled by mutual agreement, and when this is not possible by a court of law before the competent Bulgarian court under the rules of jurisdiction provided for in the Code of Civil Procedure.The nullity of individual parts of these General Terms and Conditions does not imply its total nullity when they are legally substituted by the rules of law.</p>
+            </ol>
+            <h1 style="padding-bottom: 15px;"><i>―   COOKIE POLICY   ―</i></h1>
+            <ol type="I">
+                <p>We use cookies or similar tools on our website to improve its performance and your experience. This policy explains how we do it.</p>
+                <li>What is a cookie?</li>
+                <p>A cookie is a small file consisting of a textual set of information that a website sends to a visitor\'s computer or other Internet-connected device. Cookies perform certain functions (including distinguishing you from other users on the same website or storing certain information related to you, such as your preferences) and are used by most sites to facilitate your browsing.</p>
+                <li>The types of cookies we use:</li>
+                <ul>
+                    <li>Functional cookies</li>
+                    <p>We use cookies that allow the website to store your actions and preferences for a certain period of time so that you do not have to enter them every time you visit the site or move from one page to another. These cookies do not store personal information.</p>
+                    <li>Analytical cookies</li>
+                    <p>Thanks to these cookies, we monitor the traffic to our site and can analyze how easily our users use it (Google Analytics cookies). These cookies do not store personal information. They show us which pages on our site you viewed, whether you visited our site through a mobile or desktop device and other anonymous data.</p>
+                    <li>Precision targeting cookies</li>
+                    <p>These cookies contain information about how you have used our site and may be triggered by our advertising partners. These cookies do not store personal information. Thanks to them, you will not be shown information that is irrelevant to you. These are dynamic cookies on Facebook, Google, Adform, Adwise and more.</p>
+                </ul>
+                <li>How do I delete or block cookies?</li>
+                <p>You can control and / or delete cookies whenever you like - visit aboutcookies.org for more information. You can delete all cookies already stored on your computer, and you can also set most browsers to block them. However, if you do this, you may need to manually adjust some parameters each time you visit our site, and some services and features may not work.</p>
+            </ol>
+            <h1 style="padding-bottom: 15px;"><i>―   CONFIDENTIALITY POLICY   ―</i></h1>
+            <ol type="I">
+                <li>How can you contact us about issues related to the processing of your personal data?</li>
+                <p>During working days, from 9:00 to 17:00, at the address of the company Sofia, 168 168 Tsar Boris III Blvd., Andromeda Business Center, 3rd floor, office 31 ,email:petar.emilov.petrov@gmail.com</p>
+                <p>We will review your request within 30 days of receipt.</p>
+                <li>Use of the Website</li>
+                <p>You do not need to register to use our website. If you are just a visitor to the website, we do not collect any personal information about you. If you choose to use our contact form, we will receive the personal information you have provided and in that sense we will start processing it.</p>
+                <li>What personal data do we process?</li>
+                <p>We do not collect personal information through our site, but we may obtain it if you provide it to us by sending us an email message through the contact form or email. A certain set of personal data will be required for us to be able to provide feedback to you or to respond to your request. To this end, we may process the following categories of personal data: </p>
+                <ul>
+                    <li>Name;</li>
+                    <li>contact information - telephone; address information, correspondence address, e-mail;</li>
+                </ul>
+                <p></p>
+                <p>In addition, in view of the specifics of your inquiry, we may request, respectively, to process data for: </p>
+                <ul>
+                    <li>placework;</li>
+                    <li>position;</li>
+                    <li>bank or billing information;</li>
+                </ul>
+                <p></p>
+                <p>We may collect information about the contents of emails, letters, complaints, requests, complaints that are addressed or otherwise relate to us.</p>
+                <li>Sensitive personal data (special categories of data)</li>
+                <p>We do not collect sensitive personal information through our site, and you will not be asked for any sensitive personal information through our site.</p>
+                <li>What level of security we provide for your personal data?</li>
+                <p>Max Graphic EOOD. endeavors to ensure the confidentiality and security of information, including the personal data of our employees and contractors, and persons who have provided personal information through our website. Max Graphic EOOD. continually improves technical and organizational measures, representing internal rules, policies and procedures that ensure that personal information is protected from loss, unauthorized access or disclosure.</p>
+                <li>Do we pass on your data to third parties?</li>
+                <p>In connection with the use of this website, we do not share your personal information with third parties. If necessary, Service Logistics OOD may transfer personal data concerning you to third parties in the following cases: </p>
+                <ul>
+                    <li>In the presence of a legal obligation or a request of a state body;</li>
+                    <li>Audit;</li>
+                    <li>For servicing payments under contracts or in connection with deliveries;</li>
+                    <li>In the presence of a legal dispute;</li>
+                    <li>Upon your explicit request, where applicable.</li>
+                </ul>
+                <p></p>
+                <li>How long do we process your personal data?</li>
+                <p>Information that may contain personal information and be stored for no longer than is necessary for the purposes for which the data were collected and processed. If, at a later stage, your personal data are related to the conclusion and / or performance of a contract between us, they will be processed for the duration of the contract and until the expiry of the statutory time limits for bringing a claim. The purpose is to protect our common rights and legitimate interests in the event of a possible legal dispute. Commercial and accounting information is stored for a period of 10 years in compliance with the special requirements of the law.</p>
+                <li>Do we make automated decision-making and profiling??</li>
+                <p>Max Graphic EOOD. does not perform automated decision-making against you, which has legal consequences based solely on automated processing of personal data and / or profiling. Max Graphic EOOD. does not currently profile the users of the site.</p>
+                <li>What rights do you have regarding the processing of your personal data?</li>
+                <p>Right of access<br> You have the right to request information whether we process personal data related to you.</p>
+                <p>Right of correction<br> You have the right to request that your personal data be corrected if any changes have been made to them or found to be incomplete or inaccurate.</p>
+                <p>Right of erasure (\"right to be forgotten\")<br> You have the right to request that your personal data be deleted when it is no longer needed for the purposes for which it was collected and processed, if the reason for processing your personal data is gone and there is no other legal basis; if your personal data is to be erased to comply with a legal obligation.</p>
+                <p>After considering all the circumstances of this request, we may refuse to delete your personal data if the processing is necessary to comply with our obligation under a law, a public interest task, or in the exercise and defense of legal claims.</p>
+                <p>The exercise of the right of erasure may not affect the rights and freedoms of others.</p>
+                <p>Right to restrict processing<br> You have the right to request a restriction on processing while we review your request, which challenges the accuracy of your personal data. You may request a restriction on processing when you do not want your personal data deleted in connection with the exercise or protection of legal claims. Processing restriction may also apply to your objection to automatic decision-making against you or profiling.</p>
+                <p>Right to portability<br> You have the right to receive personal data that concerns you and that you have provided to us in a structured, widely used and machine-readable format, and you have the right to request that we transfer this data to another administrator in the following cases:</p>
+                <ul>
+                    <li>Contract-based processing and automated processing: In the exercise of this right, the transfer can be made by us to another administrator only when technically feasible.This right does not apply to processing required to perform a task by public interest. The exercise of the right of portability may not affect the rights and freedoms of others.</li>
+                    <li>Right to object to automated decision-making against you: When decisions are made against you, solely on the basis of automated processing of your personal data, including when profiling, you have the right to object. This right may not be exercised when automated processing is necessary for the conclusion or performance of a contract between us.</li>
+                    <li>Right to complain: If you believe that your data protection rights have been violated, you can file a complaint with us. You can also file a complaint with the Commission for Personal Data Protection, Sofia 1592, 2 Tsvetan Lazarov Blvd., tel: +359 2 9153 518, e-mail: kzld@cpdp.bg</li>
+                </ul>
+                <p></p>
+                <li>Actuality and changes</li>
+                <p>This privacy statement may be updated by us in the event of changes to the conditions in which we process your personal data.</p>
+            </ol>
+        </section>
+    </div>
 </div>
