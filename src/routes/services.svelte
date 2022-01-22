8e16530948727400570602eb4ff430687e7c207e
Viki <viki.radeva91@gmail.com>
2022-01-22 16:56:24 +0200

    services-content

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -1,6 +1,149 @@
 <div class="services-in-title">
    <h1 class="content-title" style="color: #1F2040;">Services</h1>
 </div>
-<div class="services-main">
+<div class="services-main ">
     <h2>“CERT MEDICAL” offers a comprehensive service for ensuring compliance and CE marking of medical devices in accordance with Regulation 2017/745</h2>
+</div>
+
+<div class="services-content c-3">
+    <ol>
+        <li>
+           1. Development, implementation and preparation for certification of a quality management system according to the requirements of ISO 13485 (see also below - "Support to the overall process of conformity assessment by the Notified Body until obtaining the relevant certificate (s)");
+        </li>
+        <li>
+           2. Preparation of an initial (first) version of the Technical Specification, clearly stating that the product is a "medical device" (as defined in Regulation 2017/745);
+        </li>
+        <li>
+            3. Classification of the medical device (Annex VIII of Regulation 745) and documentation of the results of the classification;
+        </li>
+        <li>
+            4. Analysis and identification of applicable safety and performance requirements and identification of compliance measures (Annex I of Regulation 745);
+        </li>
+        <li>
+            5. Risk management in accordance with EN ISO 14971;
+            <ul>
+                <li>
+                    Development of a risk management and control process;
+                </li>
+                <li>
+                    Validation of the purpose of the product and documentation of the results;
+                </li>
+                <li>
+                    Identification and assessment of hazards, hazardous situations, predictable sequence of events based on intended use;
+                </li>
+                <li>
+                    Risk / benefit analysis. Justification of the acceptability of the residual risk;
+                </li>
+                <li>
+                    Preparation of a risk assessment report;
+                </li>
+            </ul>
+        </li>
+        <li>
+            6. Determination of harmonized standards applicable to the product;
+        </li>
+        <li>
+            7. Design / development of the medical device (development of the necessary documents describing the composition / construction of the device), verification and validation of the development of the device (documentation of the process and its results);
+            <ul>
+                <li>
+                    Identification / Description of user requirements;
+                </li>
+                <li>
+                    Preparation of input data (Assignment) for development based on the identified needs of the user;
+                </li>
+                <li>
+                    Product development - output of initial data necessary for the product to be manufactured in the future;
+                </li>
+                <li>
+                    Development of a detailed technical specification;
+                </li>
+                <li>
+                    Review and validation of the technical specification defining the target characteristics of the medical device;
+                </li>
+            </ul>
+        </li>
+        <li>
+            8. Description of the production process and production of the necessary samples
+            <ul>
+                <li>
+                    Determination of production technology;
+                </li>
+                <li>
+                    Providing the technological process;
+                </li>
+                <li>
+                    Production of samples of the medical device necessary for control, testing and clinical evaluation;
+                </li>
+                <li>
+                    Determining the risk stages of production and the relevant control points. Documentation of the production process (technology), including the assigned processing of external subcontractors;
+                </li>
+                <li>
+                    Validation and verification of a production process that ensures the production of a medical device that meets the requirements - documenting the result;
+                </li>
+            </ul>
+        </li>
+        <li>
+            9. Clinical Evaluation
+            <ul>
+                <li>
+                    Development of a process for research and analysis of clinical data;
+                </li>
+                <li>
+                    Clinical trial (clinical trial, testing or use) - development and documentation of results;
+                </li>
+                <li>
+                    Identification, research and analysis of clinical data from internal and external sources;
+                </li>
+                <li>
+                    Clinical Evaluation Report (CER) - preparation and documentation of results;
+                </li>
+            </ul>
+        </li>
+        <li>
+            10. Development and approval of packaging and packaging process
+            <ul>
+                <li>
+                    Analysis of requirements for materials, sterile barrier systems and packaging systems;
+                </li>
+                <li>
+                    Development of packaging (s), providing the necessary protection in accordance with the requirements and documenting the results of the process;
+                </li>
+                <li>
+                    Testing of the packaging system - efficiency, stability and integrity of the packaging system;
+                </li>
+                <li>
+                    Validation of the packaging process;
+                </li>
+            </ul>
+        </li>
+        <li>
+            11. Validation of the sterilization process - development and documentation of the results of the process;
+        </li>
+        <li>
+            12. Shelf life testing;
+        </li>
+        <li>
+            13. Determining the service life of the product, considerations and requirements for disposal;
+        </li>
+        <li>
+            14. Development of a label and instructions for use (CHAPTER III, item 23 of Regulation 745);
+        </li>
+        <li>
+            15. Development and approval of a Post-Market Monitoring Plan (PMS plan) - development and documentation;
+        </li>
+        <li>
+            16. Completion, review and verification of the Technical Dossier of the medical device and conducting an internal assessment of compliance with the requirements of Regulation 745;
+        </li>
+        <li>
+            17. Development of a draft EC Declaration of Conformity of the medical device;
+        </li>
+        <li>
+            18. Selection and negotiation with a Notified Body for independent assessment of compliance with the requirements of Regulation 745;
+        </li>
+        <li>
+            19. Supporting the overall conformity assessment process by the Notified Body until obtaining the relevant certificate (s);
+        </li>
+    </ol>
+</div><div class="c3">
+    <img src="/img/services.jpg" alt="">
 </div>
