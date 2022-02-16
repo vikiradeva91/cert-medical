577d1633e7a96e1c3627955dd7fb8bda02574a49
Viki <viki.radeva91@gmail.com>
2022-02-16 15:56:43 +0200

    services accordions -not ready

--- src/routes/-services.svelte
+++ src/routes/-services.svelte
@@ -1,206 +1,192 @@
 <div class="services-in-title">
 	<h1 class="content-title" style="color: #1F2040;">Services</h1>
 </div>
-<div class="services-main ">
-	<h2>Detailed list of EU MDR 745 compliance services we offer to MedTech companies</h2>
-	<div class="container-srv">
-		<section class="align-items-center row-srv">
-			<div class="col-4">
-				<img src="/img/stethoscope.png" alt="" />
-			</div>
 
-			<div class="col-6">
-				<div class="p2">
-					<h3>
-						<i>
-							I. Development and implementation of ISO 13485 - Quality Management
-							Systems (QMS) for medical devices manufacturers;
-						</i>
-					</h3>
-					<br/>
-					<p>
-						Development, implementation and preparation for certification of a quality
-						management system according to the requirements of ISO 13485 (see also below
-						- "Support to the overall process of conformity assessment by the Notified
-						Body until obtaining the relevant certificate (s)")
-					</p>
-				</div>
-			</div>
-		</section>
-	</div>
-	<div class="background">
-		<section>
-			<div>
-				<h3>
-					<i>
-						II. Technical documentation preparation, review and compliance assessment,
-						according to REGULATION (EU) 2017/745
-					</i>
-				</h3>
-				<br />
-				<p>
-					Preparation of an initial (first) version of the Technical Specification,
-					clearly stating that the product is a "medical device" (as defined in Regulation
-					2017/745);
-					<br />
-					Classification of the medical device (Annex VIII of Regulation 745) and documentation
-					of the results of the classification;
-					<br />
-					Analysis and identification of applicable safety and performance requirements and
-					identification of compliance measures (Annex I of Regulation 745);
-				</p>
-				<br />
-				<div class="grid">
-					<div>
-						<ul>
-							<b>Risk management in accordance with EN ISO 14971:</b>
-							<li>Development of a risk management and control process;</li>
-							<li>
-								Validation of the purpose of the product and documentation of the
-								results;
-							</li>
-							<li>
-								Identification and assessment of hazards, hazardous situations,
-								predictable sequence of events based on intended use;
-							</li>
-							<li>
-								Risk / benefit analysis. Justification of the acceptability of the
-								residual risk;
-							</li>
-							<li>Preparation of a risk assessment report;</li>
-						</ul>
-						<p>
-							Determination of harmonized standards and technical specification
-							applicable to the product;
-						</p>
-					</div>
+<!-- FIRST -->
+<div class="first">
+    <h1 style="font-size: 35px; text-align:center " class="p2">Detailed list of EU MDR 745 compliance services we offer to MedTech companies</h1>
 
-					<div>
-						<ul>
-							<b
-								>Design / development of the medical device (development of the
-								necessary documents describing the composition / construction of the
-								device), verification and validation of the development of the
-								device (documentation of the process and its results):</b
-							>
-							<li>Identification / Description of user requirements;</li>
-							<li>
-								Preparation of input data (Assignment) for development based on the
-								identified needs of the user;
-							</li>
-							<li>
-								Product development - output of initial data necessary for the
-								product to be manufactured in the future;
-							</li>
-							<li>Development of a detailed technical specification;</li>
-							<li>
-								Review and validation of the technical specification defining the
-								target characteristics of the medical device;
-							</li>
-						</ul>
-					</div>
+    <section>
+        <div class="first-flex p2">
+            <div>
+                <h3>
+                    <i>
+                        I. Development and implementation of ISO 13485 - Quality Management
+                        Systems (QMS) for medical devices manufacturers;
+                    </i>
+                </h3>
+                <br/>
+                <p>
+                    Development, implementation and preparation for certification of a quality
+                    management system according to the requirements of ISO 13485 (see also below
+                    - "Support to the overall process of conformity assessment by the Notified
+                    Body until obtaining the relevant certificate (s)")
+                </p>
+            </div>
 
-					<div>
-						<ul>
-							<b
-								>Description of the production process and production of the
-								necessary samples:</b
-							>
-							<li>Determination of production technology;</li>
-							<li>Providing the technological process;</li>
-							<li>
-								Production of samples of the medical device necessary for control,
-								testing and clinical evaluation;
-							</li>
-							<li>
-								Determining the risk stages of production and the relevant control
-								points. Documentation of the production process (technology),
-								including the assigned processing of external subcontractors;
-							</li>
-							<li>
-								Validation and verification of a production process that ensures the
-								production of a medical device that meets the requirements -
-								documenting the result;
-							</li>
-						</ul>
-					</div>
-
-					<div>
-						<ul>
-							<b> Development and approval of packaging and packaging process: </b>
-							<li>
-								Analysis of requirements for materials, sterile barrier systems and
-								packaging systems;
-							</li>
-							<li>
-								Development of packaging (s), providing the necessary protection in
-								accordance with the requirements and documenting the results of the
-								process;
-							</li>
-							<li>
-								Testing of the packaging system - efficiency, stability and
-								integrity of the packaging system;
-							</li>
-							<li>Validation of the packaging process;</li>
-						</ul>
-					</div>
-				</div>
-				<br />
-
-				<!-- svelte-ignore a11y-missing-attribute -->
-				<img
-					style="float: left; margin: 0px 35px 0px 0px;"
-					src="img/pic1.jpg"
-					width="550"
-				/>
-
-				<p>
-					Validation of the sterilization process - development and documentation of the
-					results of the process;
-					<br />
-					Shelf life testing;
-					<br />
-					Determining the service life of the product, considerations and requirements for
-					disposal;
-					<br />
-					Development of a label and instructions for use (CHAPTER III, item 23 of Regulation
-					745);
-					<br />
-					Development and approval of a Post-Market Monitoring Plan (PMS plan) - development
-					and documentation;
-					<br />
-					Completion, review and verification of the Technical Dossier of the medical device
-					and conducting an internal assessment of compliance with the requirements of Regulation
-					745;
-					<br />
-					EC Declaration of Conformity drafting;
-				</p>
-			</div>
-			<div style="clear: both;" />
-		</section>
-	</div>
-
-	<div class="no-color">
-		<section class="srv-flex">
-			<div>
-				<i>
-					III. Clinical evaluation process management, incl. communication and negotiation
-					with Contract Research Organizations (CRO), accredited Testing Laboratories and
-					Clinical experts
-				</i>
-				<br/><br/>
-				<p>
-					Development of a process for research and analysis of clinical data;<br/>
-					Clinical trial (clinical trial, testing or use) - development and documentation of results;<br/>
-					Identification, research and analysis of clinical data from internal and external sources;<br/>
-					Clinical Evaluation Report (CER) - preparation and documentation of results;
-
-				</p>
-			</div>
-
-			<div>
-				<img src="img/free-1.jpg" alt="">
-			</div>
-		</section>
-	</div>
+            <div>
+                <img src="/img/stethoscope2.png" alt="" />
+            </div>
+            
+        </div>
+    </section>
 </div>
+
+
+<!-- FIRST-END -->
+
+<!-- SECOND -->
+
+<div class="second">
+    <section>
+        <h3>
+            <i>
+                II. Technical documentation preparation, review and compliance assessment,
+                according to REGULATION (EU) 2017/745
+            </i>
+        </h3>
+
+        <br/>
+
+        <p>
+            Preparation of an initial (first) version of the Technical Specification,
+            clearly stating that the product is a "medical device" (as defined in Regulation
+            2017/745);
+            <br /><br />
+            Classification of the medical device (Annex VIII of Regulation 745) and documentation
+            of the results of the classification;
+            <br /><br />
+            Analysis and identification of applicable safety and performance requirements and
+            identification of compliance measures (Annex I of Regulation 745);
+        </p>
+    </section>
+
+    <section class="second-grid p2">
+        
+        <div>
+            <ul>
+                <b>Risk management in accordance with EN ISO 14971:</b>
+                <li>Development of a risk management and control process;</li>
+                <li>
+                    Validation of the purpose of the product and documentation of the
+                    results;
+                </li>
+                <li>
+                    Identification and assessment of hazards, hazardous situations,
+                    predictable sequence of events based on intended use;
+                </li>
+                <li>
+                    Risk / benefit analysis. Justification of the acceptability of the
+                    residual risk;
+                </li>
+                <li>Preparation of a risk assessment report;</li>
+                <p>
+                    Determination of harmonized standards and technical specification
+                    applicable to the product;
+                </p>
+            </ul>
+            
+        </div>
+
+        <div>
+            <ul>
+                <b
+                    >Design / development of the medical device (development of the
+                    necessary documents describing the composition / construction of the
+                    device), verification and validation of the development of the
+                    device (documentation of the process and its results):</b
+                >
+                <li>Identification / Description of user requirements;</li>
+                <li>
+                    Preparation of input data (Assignment) for development based on the
+                    identified needs of the user;
+                </li>
+                <li>
+                    Product development - output of initial data necessary for the
+                    product to be manufactured in the future;
+                </li>
+                <li>Development of a detailed technical specification;</li>
+                <li>
+                    Review and validation of the technical specification defining the
+                    target characteristics of the medical device;
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                <b
+                    >Description of the production process and production of the
+                    necessary samples:</b
+                >
+                <li>Determination of production technology;</li>
+                <li>Providing the technological process;</li>
+                <li>
+                    Production of samples of the medical device necessary for control,
+                    testing and clinical evaluation;
+                </li>
+                <li>
+                    Determining the risk stages of production and the relevant control
+                    points. Documentation of the production process (technology),
+                    including the assigned processing of external subcontractors;
+                </li>
+                <li>
+                    Validation and verification of a production process that ensures the
+                    production of a medical device that meets the requirements -
+                    documenting the result;
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                <b> Development and approval of packaging and packaging process: </b>
+                <li>
+                    Analysis of requirements for materials, sterile barrier systems and
+                    packaging systems;
+                </li>
+                <li>
+                    Development of packaging (s), providing the necessary protection in
+                    accordance with the requirements and documenting the results of the
+                    process;
+                </li>
+                <li>
+                    Testing of the packaging system - efficiency, stability and
+                    integrity of the packaging system;
+                </li>
+                <li>Validation of the packaging process;</li>
+            </ul>
+        </div>
+
+        <div>
+            <p>
+                Validation of the sterilization process - development and documentation of the
+                results of the process;
+                <br />
+                Shelf life testing;
+                <br />
+                Determining the service life of the product, considerations and requirements for
+                disposal;
+                <br />
+                Development of a label and instructions for use (CHAPTER III, item 23 of Regulation
+                745);
+                <br />
+                Development and approval of a Post-Market Monitoring Plan (PMS plan) - development
+                and documentation;
+                <br />
+                Completion, review and verification of the Technical Dossier of the medical device
+                and conducting an internal assessment of compliance with the requirements of Regulation
+                745;
+                <br />
+                EC Declaration of Conformity drafting;
+            </p>
+        </div>
+        <div class="grid-img">
+            <img src="img/pic1.jpg" alt="">
+        </div>
+    </section>
+
+</div>
+
+<!-- SECOND-END -->
+
