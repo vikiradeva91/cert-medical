f7020358745811be4dfdc932189c09d8dc918cb2
chavdar-ang <seminari365.ltd@gmail.com>
2022-06-04 12:59:05 +0300

    Service page - prismic implemented. npm update.

--- src/routes/services.svelte
+++ src/routes/services.svelte
@@ -1,3 +1,31 @@
+<script context="module">
+	import createClient from '$lib/prismicClient';
+	import * as prismicH from '@prismicio/helpers';
+
+	export async function load({ fetch, params }) {
+		const client = createClient(fetch);
+		const document = await client.getByUID('page', 'services');
+
+		console.log(document);
+
+		if (document) {
+			return {
+				props: { document }
+			};
+		}
+
+		return {
+			status: 404
+		};
+	}
+</script>
+
+<script>
+	export let document;
+
+	const services = document.data.body;
+</script>
+
 <div class="services-in-title">
 	<h1 class="content-title" style="color: #1F2040;">Services</h1>
 </div>
@@ -24,136 +52,15 @@
 		</p>
 	</div>
 	<div class="accordion-services">
-		<div class="accordion">
-			<h2>
-				I. ISO 13485 - quality management system (QMS) - development, implementation and integration.
-			</h2>
-			<div class="p">
-				<p style="padding-top:20px;">
-					ISO 13485 specifies requirements for a quality management system where an
-					organisation needs to demonstrate its ability to provide medical devices and
-					related services that consistently meet customer and applicable regulatory
-					requirements.
-				</p><br/>
-				<strong>Our service includes:</strong>
-				<ul>
-					<li>
-						Development, implementation and integration of a Quality Management System
-						(QMS) according to international standard ISO 13485 (Medical devices —
-						Quality management systems — Requirements for regulatory purposes);
-					</li>
-					<li>Team training for QMS management;</li>
-					<li>
-						Reasearch, selection and negotiation with an accredited certification body
-						(CB) - an organisation accredited by a recognized accrediting body for its
-						competence to audit and issue certification confirming that an organisation
-						meets the requirements of a standard (e.g. ISO 9001 or ISO 13485).
-					</li>
-					<li>
-						Supporting the overall certification process by the accredited certification
-						body until obtaining the ISO 13485 quality management system (QMS)
-						certificate;
-					</li>
-				</ul>
+		{#each services as service, i}
+			<div class="accordion">
+				<h2>
+					{i + 1}. {service.primary.service_short}
+				</h2>
+				<div class="p">
+					{@html prismicH.asHTML(service.primary.service_description)}
+				</div>
 			</div>
-		</div>
-
-		<div class="accordion">
-			<h2>
-				II. Technical file preparation, review and internal compliance assessment.
-			</h2>
-			<div class="p">
-				<p>
-					A Technical File is a set of documents prepared by the manufacturer to
-					demonstrate the safety and performance of the medical device, according to
-					Regulation 2017/745 and all applicable standards.
-				</p><br/>
-				<strong> Our service includes: </strong>
-				<ul>
-					<li>
-						Preparation of an initial (first) version of the Technical Specification,
-						clearly stating that the product is a "medical device" (as defined in
-						Regulation 2017/745);
-					</li>
-					<li>
-						Classification of the medical device (Annex VIII of Regulation 2017/745) and
-						documentation of the results of the classification;
-					</li>
-					<li>
-						Analysis and identification of applicable safety and performance
-						requirements and identification of compliance measures (Annex I of
-						Regulation 745);
-					</li>
-					<li>
-						Risk management process integration in accordance with EN ISO 14971. Risk
-						evaluation planning and reporting;
-					</li>
-					<li>
-						Determination of applicable harmonised standards and technical
-						specification;
-					</li>
-					<li>
-						Design / development of the medical device (development of the necessary
-						documents describing the composition / construction of the device),
-						verification and validation of the development of the device (documentation
-						of the process and its results);
-					</li>
-					<li>
-						Clinical evaluation process integration. Clinical evaluation planning and
-						reporting;
-					</li>
-					<li>
-						Product testing organisation. Accredited laboratories research, selection
-						and negotiation.&nbsp;
-					</li>
-					<li>
-						Description of the production process and production of the necessary
-						samples:
-					</li>
-					<li>Development and approval of packaging and packaging process;</li>
-					<li>Sterilisation process validation (if applicable);</li>
-					<li>Shelf life testing organisation;</li>
-					<li>
-						Determining the service life of the product, considerations and requirements
-						for disposal;
-					</li>
-					<li>Label and instructions for use preparation;</li>
-					<li>Post-Market Surveillance process and plan integration;</li>
-					<li>
-						Completion, review and verification of the Technical File of the medical
-						device and conducting an internal assessment of compliance with the
-						requirements of EU MDR 745.
-					</li>
-				</ul>
-			</div>
-		</div>
-		
-		<div class="accordion">
-			<h2>
-				III. Notified body (NB) audit support
-			</h2>
-			<div class="p">
-				<p>
-					A notified body is an organisation designated by an EU member state to assess
-					the conformity of certain products, before being placed on the EU market, with
-					the applicable essential technical requirements, for example EU MDR 745.	
-				</p><br/>
-				<strong>Our service includes:</strong>
-				<ul>
-					<li>
-						Reasearch, selection and negotiation with a designated Notified Bodies (NB);
-					</li>
-					<li>
-						Еxpert support throughout the the overall conformity assessment process
-						until obtaining the EC certificate, according to Regulation 2017/745, issued
-						by the Notified Body;
-					</li>
-					<li>Declaration of Conformity drafting;</li>
-					<li>Team Training for implementation of the MDR requirements.</li>
-				</ul>
-			</div>
-		</div>
-
+		{/each}
 	</div>
 </div>
-
